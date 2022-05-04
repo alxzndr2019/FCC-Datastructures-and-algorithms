2607ae0a44a73f0c4d9765bcc99a3ae2082a5f04
Alex Ohioze Omiunu <45854856+alxzndr2019@users.noreply.github.com>
2022-05-04 15:04:22 +0000

    Initial commit

---
+++ Stringtruncate.js
@@ -0,0 +1,10 @@
+///Question
+// Truncate a String
+// Truncate a string (first argument) if it is longer than the given maximum string length (second argument). Return the truncated string with a ... ending.
+
+//Solution
+function truncateString(str, num) {
+ return  str.length > num? str.slice(0,num)+'...' : str
+}
+
+console.log(truncateString("A-tisket a-tasket A green and yellow basket", 29));
